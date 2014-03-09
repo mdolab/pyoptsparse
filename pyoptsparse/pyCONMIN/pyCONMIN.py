6d1130e93edaaa5354b4e5a207420e62e31f9668
kenway <kenway@localhost>
2014-03-09 14:34:50 -0400

    removed mpi4py dependence. Added a built-in dummy MPI module that works the same as MPI for the functions required in pyOptSparse.

--- pyoptsparse/pyCONMIN/pyCONMIN.py
+++ pyoptsparse/pyCONMIN/pyCONMIN.py
@@ -36,7 +36,6 @@ import time
 # External Python modules
 # =============================================================================
 import numpy
-from mpi4py import MPI
 # ===========================================================================
 # Extension modules
 # ===========================================================================
@@ -252,9 +251,8 @@ class CONMIN(Optimizer):
             if iprint > 0:
                 conmin.closeunit(self.getOption('IOUT'))
 
-            if MPI:
-                # Broadcast a -1 to indcate SLSQP has finished
-                self.optProb.comm.bcast(-1, root=0)
+            # Broadcast a -1 to indcate SLSQP has finished
+            self.optProb.comm.bcast(-1, root=0)
 
             # Store Results
             sol_inform = {}
--- pyoptsparse/pyOpt_optimizer.py
+++ pyoptsparse/pyOpt_optimizer.py
@@ -22,7 +22,6 @@ import copy
 import shelve
 import numpy
 from scipy import sparse
-from mpi4py import MPI
 from .pyOpt_gradient import Gradient
 from .pyOpt_error import Error
 from .pyOpt_history import History
@@ -280,12 +279,12 @@ match the number in the current optimization. Ignorning coldStart file')
         # broadcast to know what to do.
 
         args = [x, evaluate]
-        if MPI:
-            # Broadcast the type of call (0 means regular call)
-            self.optProb.comm.bcast(0, root=0)
 
-            # Now broadcast out the required arguments:
-            self.optProb.comm.bcast(args)
+        # Broadcast the type of call (0 means regular call)
+        self.optProb.comm.bcast(0, root=0)
+
+        # Now broadcast out the required arguments:
+        self.optProb.comm.bcast(args)
 
         result = self._masterFunc2(*args)
         self.interfaceTime += time.time()-timeA
