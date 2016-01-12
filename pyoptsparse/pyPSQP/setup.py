11420177fc9561c4cc96ac1445db1c649ebd65d5
John Jasa <johnjasa11@gmail.com>
2016-01-12 14:39:02 -0500

    added line search filter for OpenMDAO hist files

---
+++ pyoptsparse/pyPSQP/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import os,sys
+
+
+def configuration(parent_package='',top_path=None):
+    
+    from numpy.distutils.misc_util import Configuration
+    
+    config = Configuration('pyPSQP',parent_package,top_path)
+    
+    config.add_library('psqp',
+        sources=[os.path.join('source', '*.f'),
+                 os.path.join('source', '*.f90')]),
+    config.add_extension('psqp',
+        sources=['source/f2py/psqp.pyf'],
+        libraries=['psqp'])
+    config.add_data_files('LICENSE','README')
+    
+    return config
