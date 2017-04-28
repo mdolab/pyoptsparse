bfab324972339f59795f35661633529a59674711
Nicolas Bons <nbons@umich.edu>
2017-04-28 18:39:31 -0400

    Some changed to mesh regeneration stuff.

--- pyoptsparse/pyOpt_optimizer.py
+++ pyoptsparse/pyOpt_optimizer.py
@@ -77,7 +77,7 @@ class Optimizer(object):
         self.userSensCalls = 0
         self.storeSens = True
         self.FailCounts = 0
-        self.MaxFailCounts = 1
+        self.MaxFailCounts = 10
         # Cache storage
         self.cache = {'x': None, 'fobj': None, 'fcon': None,
                       'gobj': None, 'gcon': None}
