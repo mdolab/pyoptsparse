b318e6c07a97436ac6bef6c33d4173d48f58524b
Neil Wu <neilwu@umich.edu>
2020-03-02 23:10:45 -0500

    added API for processing optimization history file

--- pyoptsparse/pyOpt_history.py
+++ pyoptsparse/pyOpt_history.py
@@ -65,6 +65,7 @@ class History(object):
             raise Error('The flag argument to History must be \'r\' or \'n\'.')
         self.temp = temp
         self.fileName = fileName
+        self.flag = flag
 
         # Load any keys it happens to have:
         self.keys = list(self.db.keys())
@@ -115,7 +116,7 @@ class History(object):
     def read(self, callCounter):
         """
         Read data for index 'callCounter'. Note that this
-        point should be verified by calling validPoint() first.
+        point should be verified by calling pointExists() first.
         """
         key = '%d'% callCounter
         return self.db[key]
@@ -155,6 +156,67 @@ class History(object):
             x_list.append(xuser[key])
         x_array = numpy.hstack(x_list)
         return x_array
+    
+    def getHistory(self, onlyMajor=True, deProcessX=False):
+        """
+        Parses an existing history file and returns a data dictionary used to post-process optimization results.
+
+        Parameters
+        ----------
+        onlyMajor : bool
+            flag to specify whether to include only major iterations.
+        
+        deProcessX : bool
+            flag to specify whether to return x as an OrderedDict or de-process into a single numpy array
+
+        Note that regardless of the onlyMajor flag, failed evaluations are not returned.
+        """
+        # only do this if we open the file with 'r' flag
+        if self.flag != 'r':
+            return
+        
+        # extract list of callCounters from self.keys
+        callCounters = sorted([int(x) for x in self.keys if x.isdigit()])
+
+        # set up dictionary to return
+        data = {}
+        # copy only certain keys over
+        for key in ['metadata','conInfo','objInfo','varInfo']:
+            if key in self.keys:
+                data[key] = self.readData(key)
+        
+        # pre-allocate list for each input
+        if deProcessX:
+            inputs = ['xuser']
+        else:
+            inputs = self.read(0)['xuser'].keys()
+        for input in inputs:
+            data[input] = []
+
+        # pre-allocate list for each output
+        outputs = self.read(0)['funcs'].keys()
+        for output in outputs:
+            data[output] = []
+        
+        for i in callCounters:
+            val = self.read(i)
+            if 'funcs' in val.keys(): # we have function evaluation
+                if ((onlyMajor and val['isMajor']) or not onlyMajor) and not val['fail']:
+                    for output in outputs:
+                        data[output].append(val['funcs'][output])
+                    for input in inputs:
+                        if deProcessX:
+                            data[input].append(self.deProcessX(val['xuser']))
+                        else:
+                            data[input].append(val['xuser'][input])
+                        
+        # reshape inputs and outputs into numpy arrays
+        for output in outputs:
+            data[output] = numpy.vstack(data[output])
+        for input in inputs:
+            data[input] = numpy.vstack(data[input])
+
+        return data
 
     def __del__(self):
         try:
