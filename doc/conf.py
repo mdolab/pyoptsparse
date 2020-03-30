cd64b3d8702a5ecbf0e62bc0f82e3f9a7063f040
Neil Wu <neilwu@umich.edu>
2020-03-30 11:37:33 -0400

    updated conf.py (#83)

--- doc/conf.py
+++ doc/conf.py
@@ -16,7 +16,7 @@ import sys, os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../'))
 
 # -- General configuration -----------------------------------------------------
 
@@ -92,10 +92,10 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme_path = ['themes']
-html_theme = 'mdolab_theme'
+# html_theme_path = ['themes']
+# html_theme = 'mdolab_theme'
 
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
