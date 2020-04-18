b4c3f10775e73105bbeca7e0f0c6ac980021fd11
Neil Wu <neilwu@umich.edu>
2020-04-18 14:53:57 -0400

    refactored code in test_hs071

--- test/test_hs071.py
+++ test/test_hs071.py
@@ -6,38 +6,35 @@ import unittest
 import numpy
 from pyoptsparse import Optimization, OPT
 
-
-def objfunc(xdict):
-    x = xdict['xvars']
-    funcs = {}
-    funcs['obj'] = x[0] * x[3] * (x[0] + x[1] + x[2]) + x[2]
-    funcs['con'] = [x[0] * x[1] * x[2] * x[3],
-                   x[0]*x[0] + x[1]*x[1] + x[2]*x[2] + x[3]*x[3]]
-    fail = False
-    return funcs, fail
-
-
-def sens(xdict, funcs):
-    x = xdict['xvars']
-    funcsSens = {}
-    funcsSens['obj'] = {'xvars': numpy.array(
-            [x[0] * x[3] + x[3] * (x[0] + x[1] + x[2]) ,
-             x[0] * x[3],
-             x[0] * x[3] + 1.0,
-             x[0] * (x[0] + x[1] + x[2])
-             ])}
-    jac = [[x[1]*x[2]*x[3], x[0]*x[2]*x[3], x[0]*x[1]*x[3], x[0]*x[1]*x[2]],
-           [2.0*x[0], 2.0*x[1], 2.0*x[2], 2.0*x[3]]]
-    funcsSens['con'] = {'xvars': jac}
-    fail = False
-    return funcsSens, fail
-
-
 class TestHS71(unittest.TestCase):
+    def objfunc(self, xdict):
+        x = xdict['xvars']
+        funcs = {}
+        funcs['obj'] = x[0] * x[3] * (x[0] + x[1] + x[2]) + x[2]
+        funcs['con'] = [x[0] * x[1] * x[2] * x[3],
+                    x[0]*x[0] + x[1]*x[1] + x[2]*x[2] + x[3]*x[3]]
+        fail = False
+        return funcs, fail
+
+
+    def sens(self, xdict, funcs):
+        x = xdict['xvars']
+        funcsSens = {}
+        funcsSens['obj'] = {'xvars': numpy.array(
+                [x[0] * x[3] + x[3] * (x[0] + x[1] + x[2]) ,
+                x[0] * x[3],
+                x[0] * x[3] + 1.0,
+                x[0] * (x[0] + x[1] + x[2])
+                ])}
+        jac = [[x[1]*x[2]*x[3], x[0]*x[2]*x[3], x[0]*x[1]*x[3], x[0]*x[1]*x[2]],
+            [2.0*x[0], 2.0*x[1], 2.0*x[2], 2.0*x[3]]]
+        funcsSens['con'] = {'xvars': jac}
+        fail = False
+        return funcsSens, fail
 
     def optimize(self, optName, optOptions={}, storeHistory=False, places=5, xScale=1.0, objScale=1.0, conScale=1.0):
         # Optimization Object
-        optProb = Optimization('HS071 Constraint Problem', objfunc)
+        optProb = Optimization('HS071 Constraint Problem', self.objfunc)
 
         # Design Variables
         x0 = [1.0, 5.0, 5.0, 1.0]
@@ -55,25 +52,26 @@ class TestHS71(unittest.TestCase):
         except:
             raise unittest.SkipTest('Optimizer not available:', optName)
 
-        sol = opt(optProb, sens=sens)
+        sol = opt(optProb, sens=self.sens)
 
         # Check Solution
-        self.assertAlmostEqual(sol.objectives['obj'].value, 17.0140172, places=places)
-
-        self.assertAlmostEqual(sol.xStar['xvars'][0], 1.0, places=places)
-        self.assertAlmostEqual(sol.xStar['xvars'][1], 4.743, places=places)
-        self.assertAlmostEqual(sol.xStar['xvars'][2], 3.82115, places=places)
-        self.assertAlmostEqual(sol.xStar['xvars'][3], 1.37941, places=places)
+        self.fStar = 17.0140172
+        self.xStar = (1.0, 4.743, 3.82115, 1.37941)
+        self.lambdaStar = (0.55229366, -0.16146857)
+        self.assertAlmostEqual(sol.objectives['obj'].value, self.fStar, places=places)
+        for i in range(4):
+            self.assertAlmostEqual(sol.xStar['xvars'][i], self.xStar[i], places=places)
 
         if hasattr(sol, 'lambdaStar'):
-            self.assertAlmostEqual(sol.lambdaStar['con'][0], 0.55229366, places=places)
-            self.assertAlmostEqual(sol.lambdaStar['con'][1], -0.16146857, places=places)
+            for i in range(2):
+                self.assertAlmostEqual(sol.lambdaStar['con'][i], self.lambdaStar[i], places=places)
 
     def test_snopt(self):
         self.optimize('snopt')
     
     def test_snopt_scaling(self):
         self.optimize('snopt', objScale=4.2, xScale=[2,3,4,5], conScale=[0.6, 1.7])
+        
 
     def test_slsqp(self):
         self.optimize('slsqp')
