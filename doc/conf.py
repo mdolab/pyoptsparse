1002958c0572736ffa9d64db62bce8584443abd6
Neil Wu <neilwu@umich.edu>
2020-05-31 16:15:51 -0400

    update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -47,6 +47,8 @@ templates_path = ["_templates"]
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["_build"]
 
+# The master toctree document.
+master_doc = "index"
 
 # -- Options for HTML output -------------------------------------------------
 
