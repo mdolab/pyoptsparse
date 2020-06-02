ad8a67ab44111abf2556cc28f3030a60620ccf33
Herb Schilling <hschilling@nasa.gov>
2020-06-02 08:29:38 -0400

    Added control over using mpi4py by setting environment variable PYOPTSPARSE_REQUIRE_MPI

--- pyoptsparse/pyOpt_MPI.py
+++ pyoptsparse/pyOpt_MPI.py
@@ -7,6 +7,7 @@ mpi4py. Only the method from the COMM object that are actually used in
 pyOptSparse are included here.
 """
 
+import os
 import warnings
 
 
@@ -38,13 +39,24 @@ class myMPI(object):
         self.SUM = "SUM"
         self.LOR = "OR"
 
+# Attempt to import mpi4py.
+# If PYOPTSPARSE_REQUIRE_MPI is set to a recognized positive value, attempt import
+# and raise exception on failure. If set to anything else, no import is attempted.
+if 'PYOPTSPARSE_REQUIRE_MPI' in os.environ:
+    if os.environ['PYOPTSPARSE_REQUIRE_MPI'].lower() in ['always', '1', 'true', 'yes']:
+        from mpi4py import MPI
+    else:
+        MPI = myMPI()
+# If PYOPTSPARSE_REQUIRE_MPI is unset, attempt to import mpi4py, but continue on failure
+# with a notification.
+else:
+    try:
+        from mpi4py import MPI
+    except ImportError:
+        warn = "mpi4py could not be imported. mpi4py is required to use\
+     the parallel gradient analysis and parallel objective analysis for\
+     non-gradient based optimizers. Continuing using a dummy MPI module\
+     from pyOptSparse."
+        warnings.warn(warn)
+        MPI = myMPI()
 
-try:
-    from mpi4py import MPI
-except ImportError:
-    warn = "mpi4py could not be imported. mpi4py is required to use\
- the parallel gradient analysis and parallel objective analysis for\
- non-gradient based optimizers. Continuing using a dummy MPI module\
- from pyOptSparse."
-    warnings.warn(warn)
-    MPI = myMPI()
