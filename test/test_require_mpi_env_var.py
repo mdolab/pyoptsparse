6a750c00740bd47593991c6a86a3c1139bb12550
Herb Schilling <hschilling@nasa.gov>
2020-06-04 10:04:50 -0400

    handle the case of ParOpt not being installed and running Python 2

--- test/test_require_mpi_env_var.py
+++ test/test_require_mpi_env_var.py
@@ -2,52 +2,64 @@ import importlib
 import inspect
 import unittest
 import os
+import sys
 
+if sys.version_info[0] == 2:
+    reload_func = reload
+else:
+    reload_func = importlib.reload
 
-class TestNoMPI(unittest.TestCase):
-
-    # Next 3 tests check how the environment variable affects importing MPI
+class TestRequireMPIEnvVar(unittest.TestCase):
+    # Check how the environment variable affects importing MPI
     def test_require_mpi(self):
         os.environ["PYOPTSPARSE_REQUIRE_MPI"] = "1"
         import pyoptsparse.pyOpt_MPI
 
-        importlib.reload(pyoptsparse.pyOpt_MPI)
+        reload_func(pyoptsparse.pyOpt_MPI)
         self.assertTrue(inspect.ismodule(pyoptsparse.pyOpt_MPI.MPI))
 
     def test_no_mpi_requirement_given(self):
         os.environ.pop("PYOPTSPARSE_REQUIRE_MPI", None)
         import pyoptsparse.pyOpt_MPI
 
-        importlib.reload(pyoptsparse.pyOpt_MPI)
+        reload_func(pyoptsparse.pyOpt_MPI)
         self.assertTrue(inspect.ismodule(pyoptsparse.pyOpt_MPI.MPI))
 
     def test_do_not_use_mpi(self):
         os.environ["PYOPTSPARSE_REQUIRE_MPI"] = "0"
         import pyoptsparse.pyOpt_MPI
 
-        importlib.reload(pyoptsparse.pyOpt_MPI)
+        reload_func(pyoptsparse.pyOpt_MPI)
         self.assertFalse(inspect.ismodule(pyoptsparse.pyOpt_MPI.MPI))
 
-    # Next 3 tests check how the environment variable affects using ParOpt
+class TestRequireMPIEnvVarOnParOpt(unittest.TestCase):
+    # Check how the environment variable affects using ParOpt
+    def setUp(self):
+        # Just check to see if ParOpt is installed before doing any testing
+        try:
+            from paropt import ParOpt as _ParOpt
+        except ImportError:
+            raise unittest.SkipTest("Optimizer not available:", 'paropt')
+
     def test_require_mpi_check_paropt(self):
         os.environ["PYOPTSPARSE_REQUIRE_MPI"] = "1"
         import pyoptsparse.pyParOpt.ParOpt
 
-        importlib.reload(pyoptsparse.pyParOpt.ParOpt)
+        reload_func(pyoptsparse.pyParOpt.ParOpt)
         self.assertIsNotNone(pyoptsparse.pyParOpt.ParOpt._ParOpt)
 
     def test_no_mpi_requirement_given_check_paropt(self):
         os.environ.pop("PYOPTSPARSE_REQUIRE_MPI", None)
         import pyoptsparse.pyParOpt.ParOpt
 
-        importlib.reload(pyoptsparse.pyParOpt.ParOpt)
+        reload_func(pyoptsparse.pyParOpt.ParOpt)
         self.assertIsNotNone(pyoptsparse.pyParOpt.ParOpt._ParOpt)
 
     def test_do_not_use_mpi_check_paropt(self):
         os.environ["PYOPTSPARSE_REQUIRE_MPI"] = "0"
         import pyoptsparse.pyParOpt.ParOpt
 
-        importlib.reload(pyoptsparse.pyParOpt.ParOpt)
+        reload_func(pyoptsparse.pyParOpt.ParOpt)
         self.assertIsNone(pyoptsparse.pyParOpt.ParOpt._ParOpt)
 
 
