1195a88b6ee173ba6ecd3e6abb68a6da329bbd57
Neil Wu <neilwu@umich.edu>
2020-06-06 14:11:59 -0400

    Bugfix for mpi4py import (#121)

--- test/test_require_mpi_env_var.py
+++ test/test_require_mpi_env_var.py
@@ -9,10 +9,18 @@ if sys.version_info[0] == 2:
 else:
     reload_func = importlib.reload
 
+try:
+    HAS_MPI = True
+    import mpi4py  # noqa:F401
+except ImportError:
+    HAS_MPI = False
+
 
 class TestRequireMPIEnvVar(unittest.TestCase):
     # Check how the environment variable affects importing MPI
     def test_require_mpi(self):
+        if not HAS_MPI:
+            raise unittest.SkipTest("mpi4py not available, skipping test.")
         os.environ["PYOPTSPARSE_REQUIRE_MPI"] = "1"
         import pyoptsparse.pyOpt_MPI
 
@@ -24,7 +32,10 @@ class TestRequireMPIEnvVar(unittest.TestCase):
         import pyoptsparse.pyOpt_MPI
 
         reload_func(pyoptsparse.pyOpt_MPI)
-        self.assertTrue(inspect.ismodule(pyoptsparse.pyOpt_MPI.MPI))
+        if HAS_MPI:
+            self.assertTrue(inspect.ismodule(pyoptsparse.pyOpt_MPI.MPI))
+        else:
+            self.assertFalse(inspect.ismodule(pyoptsparse.pyOpt_MPI.MPI))
 
     def test_do_not_use_mpi(self):
         os.environ["PYOPTSPARSE_REQUIRE_MPI"] = "0"
