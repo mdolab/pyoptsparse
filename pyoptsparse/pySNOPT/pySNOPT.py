2c90eefa3445046420a58f7c16daae15eb9ca793
Neil Wu <neilwu@umich.edu>
2021-01-07 23:07:02 -0500

    more overhaul of pySNOPT

--- pyoptsparse/pySNOPT/pySNOPT.py
+++ pyoptsparse/pySNOPT/pySNOPT.py
@@ -28,7 +28,7 @@ import numpy as np
 from ..pyOpt_optimizer import Optimizer
 from ..pyOpt_error import Error
 from ..pyOpt_utils import ICOL, IDATA, IROW, extractRows, mapToCSC, scaleRows
-from baseclasses.utils import CaseInsensitiveDict
+from baseclasses.utils import CaseInsensitiveSet
 
 # =============================================================================
 # SNOPT Optimizer Class
@@ -50,107 +50,27 @@ class SNOPT(Optimizer):
             "iSumm": [int, 19],  # Summary File Output Unit (override internally in snopt?)
             "Print file": [str, "SNOPT_print.out"],  # Print File Name (specified by subroutine snInit)
             "Summary file": [str, "SNOPT_summary.out"],  # Summary File Name (specified by subroutine snInit)
-            "Start": [str, "Cold"],  # has precedence over argument start, ('Warm': alternative to a cold start)
-            "Derivative level": [int, 3],  # (NOT ALLOWED IN snOptA)
+            "Start": [str, ["Cold", "Warm"]],  # used in call to snkerc, the option "Cold Start" etc are NOT allowed
+            "Derivative level": [int, 3],
+            "Proximal iterations limit": [int, 10000],  # very large # to solve proximal point problem to optimality
             "Save major iteration variables": [
                 list,
                 ["step", "merit", "feasibility", "optimality", "penalty"],
             ],  # 'Hessian', 'slack', 'lambda' and 'condZHZ' are also supported
         }
-        {
-            # SNOPT Printing Options
-            "Major print level": [int, 1],  # Majors Print (1 - line major iteration log)
-            "Minor print level": [int, 1],  # Minors Print (1 - line minor iteration log)
-            "Print frequency": [int, 100],  # Minors Log Frequency on Print File
-            "Summary frequency": [int, 100],  # Minors Log Frequency on Summary File
-            "Solution": [str, "Yes"],  # Print Solution on the Print File
-            "Suppress options listing": [type(None), None],  # (options are normally listed)
-            "System information": [str, "No"],  # Print System Information on the Print File
-            # SNOPT Problem Specification Options
-            "Problem Type": [str, "Minimize"],  # Or 'Maximize', or 'Feasible point'
-            "Objective row": [int, 1],  # row number of objective in F(x) (has precedence over ObjRow (snOptA))
-            "Infinite bound": [float, 1.0e20],  # Infinite Bound Value
-            # SNOPT Convergence Tolerances Options
-            "Major feasibility tolerance": [float, 1.0e-6],  # Target Nonlinear Constraint Violation
-            "Major optimality tolerance": [float, 1.0e-6],  # Target Complementarity Gap
-            "Minor feasibility tolerance": [float, 1.0e-6],  # For Satisfying the QP Bounds
-            # SNOPT Derivative Checking Options
-            "Verify level": [int, 0],  # Gradients Check Flag
-            "Start objective check at column": [int, 1],  # Start the gradient verification at this column
-            "Start constraint check at column": [int, 1],
-            # SNOPT Scaling Options
-            "Scale option": [int, 1],  # Scaling (1 - linear constraints and variables)
-            "Scale tolerance": [float, 0.9],  # Scaling Tolerance
-            "Scale Print": [type(None), None],  # Default: scales are not printed
-            # SNOPT Other Tolerances Options
-            "Crash tolerance": [float, 0.1],
-            "Linesearch tolerance": [float, 0.9],  # smaller for more accurate search
-            "Backoff factor": [float, 0.1],  # backtracking factor for failed evaluations
-            "Pivot tolerance": [float, 3.7e-11],  # epsilon^(2/3)
-            # SNOPT QP subproblems Options
-            "QPSolver": [str, "Cholesky"],  # Default: Cholesky
-            "Crash option": [int, 3],  # (3 - first basis is essentially triangular)
-            "Elastic mode": [str, "No"],  # (start with elastic mode until necessary)
-            "Elastic weight": [float, 1.0e4],  # (used only during elastic mode)
-            "Iterations limit": [int, 10000],  # (or 20*ncons if that is more)
-            "Partial price": [int, 1],  # (10 for large LPs)
-            # SNOPT SQP method Options
-            "Major iterations limit": [int, 1000],  # or ncons if that is more
-            "Minor iterations limit": [int, 500],  # or 3*ncons if that is more
-            "Proximal iterations limit": [int, 200],  # for solving the proximal point problem
-            "Major step limit": [float, 2.0],
-            "Superbasics limit": [int, None],  # (n1 + 1, n1 = number of nonlinear variables)
-            "Derivative option": [int, 1],  # (ONLY FOR snOptA)
-            "Derivative linesearch": [type(None), None],
-            "Nonderivative linesearch": [type(None), None],
-            "Function precision": [float, 3.0e-13],  # epsilon^0.8 (almost full accuracy)
-            "Difference interval": [float, 5.5e-7],  # Function precision^(1/2)
-            "Central difference interval": [float, 6.7e-5],  # Function precision^(1/3)
-            "New superbasics limit": [int, 99],  # controls early termination of QPs
-            "Penalty parameter": [float, 0.0],  # initial penalty parameter
-            "Proximal point method": [int, 1],  # (1 - satisfies linear constraints near x0)
-            "Reduced Hessian dimension": [int, 2000],  # (or Superbasics limit if that is less)
-            "Violation limit": [float, 10.0],  # (unscaled constraint violation limit)
-            "Unbounded step size": [float, 1.0e18],
-            "Unbounded objective": [float, 1.0e15],
-            # SNOPT Hessian approximation Options
-            "Hessian full memory": [type(None), None],  # default if n1 <= 75
-            "Hessian limited memory": [type(None), None],  # default if n1 > 75
-            "Hessian frequency": [int, 999999],  # for full Hessian (never reset)
-            "Hessian updates": [int, 10],  # for limited memory Hessian
-            "Hessian flush": [int, 999999],  # no flushing
-            # SNOPT Frequencies Options
-            "Check frequency": [int, 60],  # test row residuals ||Ax - sk||
-            "Expand frequency": [int, 10000],  # for anti-cycling procedure
-            "Factorization frequency": [int, 50],  # 100 for LPs
-            "Save frequency": [int, 100],  # save basis map
-            # SNOPT LUSOL Options
-            "LU factor tolerance": [float, 3.99],  # for NP (100.0 for LP)
-            "LU update tolerance": [float, 3.99],  # for NP ( 10.0 for LP)
-            "LU singularity tolerance": [float, 3.2e-11],
-            "LU partial pivoting": [type(None), None],  # default threshold pivoting strategy
-            "LU rook pivoting": [type(None), None],  # threshold rook pivoting
-            "LU complete pivoting": [type(None), None],  # threshold complete pivoting
-            # SNOPT Basis files Options
-            "Old basis file": [int, 0],  # input basis map
-            "New basis file": [int, 0],  # output basis map
-            "Backup basis file": [int, 0],  # output extra basis map
-            "Insert file": [int, 0],  # input in industry format
-            "Punch file": [int, 0],  # output Insert data
-            "Load file": [int, 0],  # input names and values
-            "Dump file": [int, 0],  # output Load data
-            "Solution file": [int, 0],  # different from printed solution
-            # SNOPT Partitions of cw, iw, rw Options
-            "Total character workspace": [int, 500],  # lencw: 500
-            "Total integer workspace": [int, None],  # leniw: 500 + 100 * (m+n)
-            "Total real workspace": [int, None],  # lenrw: 500 + 200 * (m+n)
-            "User character workspace": [int, 500],
-            "User integer workspace": [int, 500],
-            "User real workspace": [int, 500],
-            # SNOPT Miscellaneous Options
-            "Debug level": [int, 1],  # (0 - Normal, 1 - for developers)
-            "Timing level": [int, 3],  # (3 - print cpu times)
-        }
+        # these are SNOPT-related options that do not get set via snset
+        self.specialOptions = CaseInsensitiveSet(
+            {
+                "iPrint",
+                "iSumm",
+                "Start",
+                "Print file",
+                "Summary file",
+            }
+        )
+        # this is purely within pySNOPT, nothing to do with SNOPT itself
+        self.pythonOptions = CaseInsensitiveSet({"Save major iteration variables"})
+
         self.informs = {
             0: "finished successfully",
             1: "optimality conditions satisfied",
@@ -228,12 +148,6 @@ class SNOPT(Optimizer):
             if raiseError:
                 raise Error("There was an error importing the compiled snopt module")
 
-        # any default options modified by pySNOPT goes here
-        self.set_options = CaseInsensitiveDict(
-            {
-                "Proximal iterations limit": 10000,  # very large # to solve proximal point problem to optimality
-            }
-        )
         super().__init__(
             name,
             category=category,
@@ -676,30 +590,20 @@ class SNOPT(Optimizer):
         Set all the options into SNOPT that have been assigned
         by the user
         """
-
         # Set Options from the local options dictionary
         # ---------------------------------------------
         inform = np.array([-1], np.intc)
-        for name, value in self.set_options.items():
-            if name == "iprint" or name == "isumm":
+        for name, value in self.options.items():
+            # these do not get set using snset
+            if name in self.specialOptions or name in self.pythonOptions:
                 continue
 
             if isinstance(value, str):
-                if name == "start":
-                    if value == "Cold":
-                        snopt.snset("Cold start", iPrint, iSumm, inform, cw, iw, rw)
-                    elif value == "Warm":
-                        snopt.snset("Warm start", iPrint, iSumm, inform, cw, iw, rw)
-                elif name == "problem type":
-                    if value == "Minimize":
-                        snopt.snset("Minimize", iPrint, iSumm, inform, cw, iw, rw)
-                    elif value == "Maximize":
-                        snopt.snset("Maximize", iPrint, iSumm, inform, cw, iw, rw)
-                    elif value == "Feasible point":
-                        snopt.snset("Feasible point", iPrint, iSumm, inform, cw, iw, rw)
-                elif name == "print file":
+                if name == "Problem Type":
+                    snopt.snset(value, iPrint, iSumm, inform, cw, iw, rw)
+                elif name == "Print file":
                     snopt.snset(name + " " + "%d" % iPrint, iPrint, iSumm, inform, cw, iw, rw)
-                elif name == "summary file":
+                elif name == "Summary file":
                     snopt.snset(name + " " + "%d" % iSumm, iPrint, iSumm, inform, cw, iw, rw)
                 else:
                     snopt.snset(name + " " + value, iPrint, iSumm, inform, cw, iw, rw)
@@ -711,20 +615,6 @@ class SNOPT(Optimizer):
                 snopt.snset(name, iPrint, iSumm, inform, cw, iw, rw)
 
         return
-
-    def _on_setOption(self, name, value):
-        """
-        Set Optimizer Option Value (Optimizer Specific Routine)
-        """
-
-        self.set_options[name] = value
-
-    def _on_getOption(self, name):
-        """
-        Get Optimizer Option Value (Optimizer Specific Routine)
-        """
-
-        pass
 
     def _on_getInform(self, infocode):
         """
