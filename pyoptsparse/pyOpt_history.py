f1d9745ac2aa33955ed8b26157a6c4564f436628
Neil Wu <neilwu@umich.edu>
2021-03-19 20:33:55 -0400

    allow iterDict to have no funcs

--- pyoptsparse/pyOpt_history.py
+++ pyoptsparse/pyOpt_history.py
@@ -454,17 +454,22 @@ class History(object):
         These are all "flat" dictionaries, with simple key:value pairs.
         """
         conDict = {}
-        for con in list(self.optProb.constraints.keys()):
-            # linear constraints are not stored in funcs
-            if not self.optProb.constraints[con].linear:
-                conDict[con] = d["funcs"][con]
-            else:
-                # the linear constraints are removed from optProb so that scaling works
-                # without needing the linear constraints to be present
-                self.optProb.constraints.pop(con)
         objDict = {}
-        for obj in self.objNames:
-            objDict[obj] = d["funcs"][obj]
+        # these require funcs which may not always be there
+        if "funcs" in d.keys():
+            for con in list(self.optProb.constraints.keys()):
+                # linear constraints are not stored in funcs
+                if not self.optProb.constraints[con].linear:
+                    conDict[con] = d["funcs"][con]
+                else:
+                    # the linear constraints are removed from optProb so that scaling works
+                    # without needing the linear constraints to be present
+                    self.optProb.constraints.pop(con)
+
+            for obj in self.objNames:
+                objDict[obj] = d["funcs"][obj]
+
+        # the DVs will always be there
         DVDict = {}
         for DV in self.DVNames:
             DVDict[DV] = d["xuser"][DV]
