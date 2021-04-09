18836a0228d138c3e0299fd0b1d98db9e3766c97
lamkina <lamkin@umich.edu>
2021-04-08 20:56:38 -0400

    Added custom switches and state control

--- pyoptsparse/postprocessing/sub_window/view.py
+++ pyoptsparse/postprocessing/sub_window/view.py
@@ -17,6 +17,7 @@ from PyQt5 import QtWidgets, QtCore
 # ==============================================================================
 from .utils.combo_box import ExtendedComboBox
 from .utils.button import Button
+from .utils.switch import Switch
 from .plot_view import PlotView
 from .controller import SubWindowController
 from .plot_controller import PlotController
@@ -49,13 +50,13 @@ class SubWindowView(QtWidgets.QWidget):
         y_layout = QtWidgets.QVBoxLayout()
         sub_layout.addLayout(y_layout)
 
-        # --- Create sublayout for LHS options ---
+        # --- Create sublayout for options ---
         opt1_layout = QtWidgets.QVBoxLayout()
         sub_layout.addLayout(opt1_layout)
 
-        # --- Create sublayout for RHS options ---
-        opt2_layout = QtWidgets.QVBoxLayout()
-        sub_layout.addLayout(opt2_layout)
+        # --- Create sublayout for buttons ---
+        button_layout = QtWidgets.QVBoxLayout()
+        sub_layout.addLayout(button_layout)
 
         # ==============================================================================
         # X Variable Layout - Left Center column of Sub Layout
@@ -65,8 +66,7 @@ class SubWindowView(QtWidgets.QWidget):
         self.x_cbox.setToolTip("Type to search for x-variables")
         self.x_cbox.setInsertPolicy(QtWidgets.QComboBox.NoInsert)
         self.x_cbox.resize(250, 30)
-        self.x_cbox.addItems(["test1", "test2", "test3"])
-        self.x_cbox.currentIndexChanged.connect(self._controller.addVarX)
+        self.x_cbox.activated.connect(self._controller.addVarX)
         x_layout.addWidget(self.x_cbox)
 
         # --- Add x-vars variable list ---
@@ -95,8 +95,7 @@ class SubWindowView(QtWidgets.QWidget):
         self.y_cbox.setInsertPolicy(QtWidgets.QComboBox.NoInsert)
         self.y_cbox.setToolTip("Type to search for y-variables")
         self.y_cbox.resize(250, 30)
-        self.y_cbox.addItems(["test1", "test2", "test3"])
-        self.y_cbox.currentIndexChanged.connect(self._controller.addVarY)
+        self.y_cbox.activated.connect(self._controller.addVarY)
         y_layout.addWidget(self.y_cbox)
 
         # --- Add y-vars variable list ---
@@ -118,58 +117,83 @@ class SubWindowView(QtWidgets.QWidget):
         y_layout.addWidget(self.y_clear_btn)
 
         # ==============================================================================
-        # Options Layout 1 - First sub-layout column for options
+        # Options Layout - Sub-layout column for options
         # ==============================================================================
         # --- Stacked Plots ---
         self.stack_plot_opt = QtWidgets.QCheckBox("Stack plots")
-        self.stack_plot_opt.clicked.connect(self._controller.stackPlots)
-        opt1_layout.addWidget(self.stack_plot_opt)
-        opt1_layout.setAlignment(self.stack_plot_opt, QtCore.Qt.AlignLeft)
+        opt1_layout.addWidget(self.stack_plot_opt, QtCore.Qt.AlignLeft)
 
-        # --- Shared y-axis ---
-        self.share_y_opt = QtWidgets.QCheckBox("Shared y-axis")
-        self.share_y_opt.clicked.connect(self._controller.shareAxisY)
-        opt1_layout.addWidget(self.share_y_opt)
-        opt1_layout.setAlignment(self.share_y_opt, QtCore.Qt.AlignLeft)
+        # --- Shared x-axis ---
+        self.share_x_opt = QtWidgets.QCheckBox("Shared x-axis")
+        opt1_layout.addWidget(self.share_x_opt, QtCore.Qt.AlignLeft)
 
         # --- y-axis as absolute delta values ---
         self.abs_delta_opt = QtWidgets.QCheckBox("y-axis as absolute delta values")
-        self.abs_delta_opt.clicked.connect(self._controller.absDeltaY)
-        opt1_layout.addWidget(self.abs_delta_opt)
-        opt1_layout.setAlignment(self.abs_delta_opt, QtCore.Qt.AlignLeft)
+        opt1_layout.addWidget(self.abs_delta_opt, QtCore.Qt.AlignLeft)
 
         # --- x-axis as minor iterations ---
         self.minor_itr_opt = QtWidgets.QCheckBox("x-axis as minor iterations")
-        self.minor_itr_opt.clicked.connect(self._controller.minorIterX)
-        opt1_layout.addWidget(self.minor_itr_opt)
-        opt1_layout.setAlignment(self.minor_itr_opt, QtCore.Qt.AlignLeft)
+        opt1_layout.addWidget(self.minor_itr_opt, QtCore.Qt.AlignLeft)
 
         # --- x-axis as major iterations ---
         self.major_itr_opt = QtWidgets.QCheckBox("x-axis as major iterations")
         self.major_itr_opt.clicked.connect(self._controller.majorIterX)
-        opt1_layout.addWidget(self.major_itr_opt)
-        opt1_layout.setAlignment(self.major_itr_opt, QtCore.Qt.AlignLeft)
+        opt1_layout.addWidget(self.major_itr_opt, QtCore.Qt.AlignLeft)
 
-        # --- Apply scaling factor ---
-        self.scale_factor_opt = QtWidgets.QCheckBox("Apply Scaling Factor")
-        self.scale_factor_opt.clicked.connect(self._controller.scaleFactor)
-        opt1_layout.addWidget(self.scale_factor_opt)
-        opt1_layout.setAlignment(self.scale_factor_opt, QtCore.Qt.AlignLeft)
-
-        # ==============================================================================
-        # Options Layout 2 - Second sub-layout column for options
-        # ==============================================================================
         # --- Min/Max arrays ---
         self.min_max_opt = QtWidgets.QCheckBox("Min/Max for arrays")
-        self.min_max_opt.clicked.connect(self._controller.minMaxPlot)
-        opt2_layout.addWidget(self.min_max_opt)
-        opt2_layout.setAlignment(self.min_max_opt, QtCore.Qt.AlignLeft)
+        opt1_layout.addWidget(self.min_max_opt, QtCore.Qt.AlignLeft)
 
-        # --- Auto refresh data ---
-        self.auto_refresh_opt = QtWidgets.QCheckBox("Automatically refresh history")
-        self.auto_refresh_opt.clicked.connect(self._controller.autoRefresh)
-        opt2_layout.addWidget(self.auto_refresh_opt)
-        opt2_layout.setAlignment(self.auto_refresh_opt, QtCore.Qt.AlignLeft)
+        # --- Variable bounds and constraints ---
+        self.bound_opt = QtWidgets.QCheckBox("Show variable/function bounds")
+        opt1_layout.addWidget(self.bound_opt, QtCore.Qt.AlignLeft)
+
+        # ==============================================================================
+        # Button Layout - Sub-layout column for buttons
+        # ==============================================================================
+        # --- Add file ---
+        self.add_file_btn = Button("Add file", self)
+        self.add_file_btn.clicked.connect(self._controller.addFile)
+        button_layout.addWidget(self.add_file_btn)
+
+        # --- Refresh history file ---
+        self.refresh_btn = Button("Refresh History File", self)
+        self.refresh_btn.clicked.connect(self._controller.refreshFile)
+        button_layout.addWidget(self.refresh_btn)
+
+        # --- Plot ---
+        self.plot_btn = Button("Plot", self)
+        self.plot_btn.clicked.connect(self._controller.plot)
+        button_layout.addWidget(self.plot_btn)
+
+        # --- Clear Plot ---
+        self.clear_plot_btn = Button("Clear Plot", self)
+        self.clear_plot_btn.clicked.connect(self._controller.clearPlot)
+        button_layout.addWidget(self.clear_plot_btn)
+
+        # ==============================================================================
+        # Switch Layout - Sub-layout rows of button layout for toggleable options
+        # ==============================================================================
+        # --- create the scale variables toggle ---
+        scale_layout = QtWidgets.QHBoxLayout()
+        button_layout.addLayout(scale_layout)
+        self.scale_var_togg = Switch(self)
+        self.scale_var_lbl = QtWidgets.QLabel("Apply Scaling Factor")
+        self.scale_var_lbl.setBuddy(self.scale_var_togg)
+        scale_layout.addWidget(self.scale_var_lbl)
+        scale_layout.addWidget(self.scale_var_togg, alignment=QtCore.Qt.AlignRight)
+
+        # --- create the auto-refresh toggle ---
+        refresh_layout = QtWidgets.QHBoxLayout()
+        button_layout.addLayout(refresh_layout)
+        self.auto_refresh_togg = Switch(self)
+        self.auto_refresh_lbl = QtWidgets.QLabel("Auto Refresh History File")
+        self.auto_refresh_lbl.setBuddy(self.auto_refresh_togg)
+        refresh_layout.addWidget(self.auto_refresh_lbl)
+        refresh_layout.addWidget(self.auto_refresh_togg, alignment=QtCore.Qt.AlignRight)
+
+        # --- Set initial UI states ---
+        self._controller.setInitialState()
 
         # --- Set the main layout ---
         self.setLayout(layout)
