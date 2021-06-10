dc5ebe6001dccbc7845f94184909ca9df1988d77
lamkina <lamkin@umich.edu>
2021-06-09 20:28:52 -0400

    Moved the variable list widget into different location and replaced the x/y variable display with a single variable display.  x axis will now be major/minor iterations only.

--- pyoptsparse/postprocessing/sub_MVCs/configure_plot_window/configure_view.py
+++ pyoptsparse/postprocessing/sub_MVCs/configure_plot_window/configure_view.py
@@ -10,40 +10,13 @@ Window view for each tab.
 # ==============================================================================
 # External Python modules
 # ==============================================================================
-from PyQt5 import QtWidgets, QtCore
+from PyQt5 import QtWidgets
 
 # ==============================================================================
 # Extension modules
 # ==============================================================================
 from pyoptsparse.postprocessing.utils.combo_box import ExtendedComboBox
 from pyoptsparse.postprocessing.utils.button import Button
-from pyoptsparse.postprocessing.utils.switch import Switch
-
-
-class VariableListWidget(QtWidgets.QWidget):
-    def __init__(self, var_name: str = "Default Name", idx: int = 0, axis: str = "x", parent=None, controller=None):
-        super(VariableListWidget, self).__init__(parent)
-
-        self.controller = controller
-
-        self.idx = idx
-
-        self.axis = axis
-
-        self.label = QtWidgets.QLabel(var_name)
-
-        self.remove_button = QtWidgets.QPushButton("X")
-        self.remove_button.clicked.connect(self.remove)
-
-        layout = QtWidgets.QHBoxLayout()
-
-        layout.addWidget(self.label, 10)
-        layout.addWidget(self.remove_button, 1)
-
-        self.setLayout(layout)
-
-    def remove(self):
-        self.controller.remove_variable(self.idx, self.axis)
 
 
 class ConfigurePlotView(QtWidgets.QDialog):
@@ -67,10 +40,6 @@ class ConfigurePlotView(QtWidgets.QDialog):
         top_left_layout = QtWidgets.QVBoxLayout()
         top_layout.addLayout(top_left_layout, 1)
 
-        # --- Create sub layout for x-variables ---
-        top_center_layout = QtWidgets.QVBoxLayout()
-        top_layout.addLayout(top_center_layout, 2)
-
         # --- Create sub layout for y-variables ---
         top_right_layout = QtWidgets.QVBoxLayout()
         top_layout.addLayout(top_right_layout, 2)
@@ -80,9 +49,6 @@ class ConfigurePlotView(QtWidgets.QDialog):
 
         mid_left_layout = QtWidgets.QVBoxLayout()
         mid_layout.addLayout(mid_left_layout)
-
-        mid_center_layout = QtWidgets.QVBoxLayout()
-        mid_layout.addLayout(mid_center_layout)
 
         mid_right_layout = QtWidgets.QVBoxLayout()
         mid_layout.addLayout(mid_right_layout)
@@ -96,64 +62,24 @@ class ConfigurePlotView(QtWidgets.QDialog):
         self._controller.populate_files()  # This must be called after file list is instantiated
 
         # ==============================================================================
-        # X Variables - Top Center Layout
-        # ==============================================================================
-        # --- Add x-vars combobox ---
-        self.x_cbox = ExtendedComboBox(self)
-        self.x_cbox.setToolTip("Type to search for x-variables")
-        self.x_cbox.setInsertPolicy(QtWidgets.QComboBox.NoInsert)
-        self.x_cbox.activated.connect(self._controller.add_x_var)
-        top_center_layout.addWidget(self.x_cbox)
-
-        # --- Add x-vars variable list ---
-        self.x_list = QtWidgets.QListWidget(self)
-        top_center_layout.addWidget(self.x_list)
-
-        # ==============================================================================
-        # Y Variables - Top Right Layout
+        # Variables - Top Right Layout
         # ==============================================================================
         # --- Add y-vars combobox ---
-        self.y_cbox = ExtendedComboBox(self)
-        self.y_cbox.setInsertPolicy(QtWidgets.QComboBox.NoInsert)
-        self.y_cbox.setToolTip("Type to search for y-variables")
-        self.y_cbox.activated.connect(self._controller.add_y_var)
-        top_right_layout.addWidget(self.y_cbox)
+        self.var_cbox = ExtendedComboBox(self)
+        self.var_cbox.setInsertPolicy(QtWidgets.QComboBox.NoInsert)
+        self.var_cbox.setToolTip("Type to search for y-variables")
+        self.var_cbox.activated.connect(self._controller.add_var)
+        top_right_layout.addWidget(self.var_cbox)
 
         # --- Add y-vars variable list ---
-        self.y_list = QtWidgets.QListWidget(self)
-        top_right_layout.addWidget(self.y_list)
+        self.var_list = QtWidgets.QListWidget(self)
+        top_right_layout.addWidget(self.var_list)
 
         # ==============================================================================
         # Options Layout 1 - Mid Left Layout
         # ==============================================================================
-        self.bounds_opt = QtWidgets.QCheckBox("Y-Variable Bounds")
-        self.bounds_opt.clicked.connect(self._controller.bounds_opt_checked)
-        mid_left_layout.addWidget(self.bounds_opt)
-
-        self.scale_opt = QtWidgets.QCheckBox("Scale Y-Variables")
-        self.scale_opt.clicked.connect(self._controller.scale_opt_checked)
-        mid_left_layout.addWidget(self.scale_opt)
-
-        # ==============================================================================
-        # Options Layout 2 -  Mid Center Layout
-        # ==============================================================================
-        major_iter_layout = QtWidgets.QHBoxLayout()
-        mid_center_layout.addLayout(major_iter_layout)
-        self.major_iter_togg = Switch(self)
-        self.major_iter_togg.clicked.connect(self._controller.major_iter_toggled)
-        self.major_iter_lbl = QtWidgets.QLabel("X-Axis as Major Iterations")
-        self.major_iter_lbl.setBuddy(self.major_iter_togg)
-        major_iter_layout.addWidget(self.major_iter_lbl)
-        major_iter_layout.addWidget(self.major_iter_togg, alignment=QtCore.Qt.AlignRight)
-
-        minor_iter_layout = QtWidgets.QHBoxLayout()
-        mid_center_layout.addLayout(minor_iter_layout)
-        self.minor_iter_togg = Switch(self)
-        self.minor_iter_togg.clicked.connect(self._controller.minor_iter_toggled)
-        self.minor_iter_lbl = QtWidgets.QLabel("X-Axis as Minor Iterations")
-        self.minor_iter_lbl.setBuddy(self.minor_iter_togg)
-        minor_iter_layout.addWidget(self.minor_iter_lbl)
-        minor_iter_layout.addWidget(self.minor_iter_togg, alignment=QtCore.Qt.AlignRight)
+        self.all_vars_list = QtWidgets.QListWidget(self)
+        mid_left_layout.addWidget(self.all_vars_list)
 
         # ==============================================================================
         # Button Layout - Mid right Layout
