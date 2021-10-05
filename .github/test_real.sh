c98c904c6bf8725584899fbf81e3198ea3bdb141
Neil Wu <602725+nwu63@users.noreply.github.com>
2021-10-05 14:06:38 -0400

    Update testing (#272)

--- .github/test_real.sh
+++ .github/test_real.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 set -e
 
-# all tests should pass on private
+# all tests should pass on the private image
 if [[ $IMAGE == "private" ]]; then
     EXTRA_FLAGS='--disallow_skipped'
 fi
 
+cd tests
+# we have to copy over the coveragerc file to make sure it's in the
+# same directory where codecov is run
+cp ../.coveragerc .
 testflo --pre_announce -v --coverage --coverpkg pyoptsparse $EXTRA_FLAGS
