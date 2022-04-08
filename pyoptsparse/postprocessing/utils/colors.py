745480c2e70144e531522176272931ed684ed0e6
Andrew Lamkin <lamkina@umich.edu>
2022-04-08 16:36:05 -0400

    Added QtGui colors to their own file

---
+++ pyoptsparse/postprocessing/utils/colors.py
@@ -0,0 +1,7 @@
+# External modules
+from PyQt5 import QtGui
+
+# --- Color constants ---
+GREEN = QtGui.QColor(0, 255, 0, 20)
+BLUE = QtGui.QColor(0, 150, 255, 20)
+WHITE = QtGui.QColor(255, 255, 255)
