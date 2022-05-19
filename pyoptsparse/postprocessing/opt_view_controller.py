0b3f13800ac82568f7f293cdd8abd3e7670ae0a7
Andrew Lamkin <lamkina@umich.edu>
2022-05-19 09:47:45 -0400

    Added a function to open the settings sub window

--- pyoptsparse/postprocessing/opt_view_controller.py
+++ pyoptsparse/postprocessing/opt_view_controller.py
@@ -2,17 +2,19 @@
 from typing import List
 
 # External modules
-from PyQt5.QtWidgets import QApplication, QInputDialog, QLineEdit, QWidget
+from PyQt5.QtWidgets import QApplication, QInputDialog, QLineEdit
 import qdarkstyle
 
 # First party modules
+from pyoptsparse.postprocessing.sub_windows.settings_window.settings_controller import SettingsController
+from pyoptsparse.postprocessing.sub_windows.settings_window.settings_view import SettingsView
 from pyoptsparse.postprocessing.sub_windows.tab_window.tab_controller import TabController
 from pyoptsparse.postprocessing.sub_windows.tab_window.tab_view import TabView
 from pyoptsparse.postprocessing.utils.base_classes import Controller
 
 
 class OptViewController(Controller):
-    def __init__(self, view: QWidget):
+    def __init__(self):
         """
         Main OptView controller.  Communicates between the main view and
         the different plotting tabs created by the user.
@@ -23,7 +25,6 @@ class OptViewController(Controller):
             MainView instance.
         """
         super(OptViewController, self).__init__()
-        self._view = view
         self._dark_mode = False
 
     def addTab(self, interactive: bool = True, tab_name: str = "Home", file_names: List = []):
@@ -31,10 +32,10 @@ class OptViewController(Controller):
         Adds a tab to the main view.
         """
         if interactive:
-            tab_name, ok_pressed = QInputDialog.getText(self._view, "Enter Tab Name", "Tab Name:", QLineEdit.Normal, "")
-        tab_controller = TabController(self._view, file_names=file_names)
-        tab_view = TabView(self._view, tab_controller)
-        self._view.tabs.addTab(tab_view, tab_name)
+            tab_name, ok_pressed = QInputDialog.getText(self.view, "Enter Tab Name", "Tab Name:", QLineEdit.Normal, "")
+        tab_controller = TabController(self.view, file_names=file_names)
+        tab_view = TabView(self.view, tab_controller)
+        self.view.tabs.addTab(tab_view, tab_name)
 
     def closeTab(self, current_index: int):
         """
@@ -45,7 +46,7 @@ class OptViewController(Controller):
         current_index : int
             The index of the current tab.
         """
-        self._view.tabs.removeTab(current_index)
+        self.view.tabs.removeTab(current_index)
 
     def toggleDarkMode(self):
         app = QApplication.instance()
@@ -54,3 +55,6 @@ class OptViewController(Controller):
             app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
         else:
             app.setStyleSheet("")
+
+    def configure_settings(self):
+        SettingsView(parent=self.view, controller=SettingsController())
