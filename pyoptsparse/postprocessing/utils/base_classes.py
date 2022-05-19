14fd5a484a181449dc7948fdd4ac891efe87757f
Andrew Lamkin <lamkina@umich.edu>
2022-05-19 09:48:46 -0400

    Abstracted the view property to be a common attribute for all controllers

--- pyoptsparse/postprocessing/utils/base_classes.py
+++ pyoptsparse/postprocessing/utils/base_classes.py
@@ -1,9 +1,21 @@
+# External modules
+from PyQt5.QtWidgets import QWidget
+
+
 class Controller(object):
     def __init__(self, *args, **kwargs):
         """
         Base class for controller objects.
         """
-        pass
+        self._view = None
+
+    @property
+    def view(self):
+        return self._view
+
+    @view.setter
+    def view(self, view: QWidget):
+        self._view = view
 
 
 class Model(object):
