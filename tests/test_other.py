b08efe4f7591fdfa5d1b45ed99808070c62755be
Phil Chiu <phil.chiu@archer.com>
2023-05-01 07:22:39 -0600

    black

--- tests/test_other.py
+++ tests/test_other.py
@@ -11,7 +11,6 @@ class TestImportSnoptFromPath(unittest.TestCase):
         with self.assertWarns(ImportWarning):
             self.assertIsNone(_import_snopt_from_path("/a/nonexistent/path"))
 
-
     def test_sys_path_unchanged(self):
         path = tuple(sys.path)
         _import_snopt_from_path("/some/path")
