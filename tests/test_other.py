fd44a5fc25706efe5075d03e0d74863071664b82
Neil Wu <nwu@archer.com>
2023-05-19 14:27:10 -0700

    fixed SNOPT import test

--- tests/test_other.py
+++ tests/test_other.py
@@ -1,9 +1,14 @@
 # Standard Python modules
+import os
 import sys
 import unittest
 
+# we have to unset this environment variable because otherwise when we import `_import_snopt_from_path`
+# the snopt module gets automatically imported, thus failing the import test below
+os.environ.pop("PYOPTSPARSE_IMPORT_SNOPT_FROM", None)
+
 # First party modules
-from pyoptsparse.pySNOPT.pySNOPT import _import_snopt_from_path
+from pyoptsparse.pySNOPT.pySNOPT import _import_snopt_from_path  # noqa: E402
 
 
 class TestImportSnoptFromPath(unittest.TestCase):
