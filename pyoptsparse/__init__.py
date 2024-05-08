4ed7ce953eed8a1955e51dc2ab5b4e7a2806df05
Ella Wu <602725+ewu63@users.noreply.github.com>
2024-05-08 18:54:21 -0400

    Fix empty hist (#399)
    * add failing test
    
    * fix reshaping
    
    * version bump
    
    * add __all__

--- pyoptsparse/__init__.py
+++ pyoptsparse/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "2.11.0"
+__version__ = "2.11.1"
 
 from .pyOpt_history import History
 from .pyOpt_variable import Variable
@@ -19,3 +19,25 @@ from .pyNLPQLP.pyNLPQLP import NLPQLP
 from .pyNSGA2.pyNSGA2 import NSGA2
 from .pyALPSO.pyALPSO import ALPSO
 from .pyParOpt.ParOpt import ParOpt
+
+__all__ = [
+    "History",
+    "Variable",
+    "Gradient",
+    "Constraint",
+    "Objective",
+    "Optimization",
+    "Optimizer",
+    "OPT",
+    "Optimizers",
+    "Solution",
+    "SNOPT",
+    "IPOPT",
+    "SLSQP",
+    "CONMIN",
+    "PSQP",
+    "NLPQLP",
+    "NSGA2",
+    "ALPSO",
+    "ParOpt",
+]
