bbc1c6fda1f152bb17f0e02544dd45b3c7bad199
Andrew Ellis <andrew.ellis7228@gmail.com>
2024-09-19 20:44:52 -0400

    added warning

--- pyoptsparse/pySLSQP/pySLSQP.py
+++ pyoptsparse/pySLSQP/pySLSQP.py
@@ -7,6 +7,7 @@ work with sparse optimization problems.
 import datetime
 import os
 import time
+import warnings
 
 # External modules
 import numpy as np
@@ -166,6 +167,9 @@ class SLSQP(Optimizer):
             # SLSQP - Objective/Constraint Values Function
             # =================================================================
             def slfunc(m, me, la, n, f, g, x):
+                if (x < blx).any() or (x > bux).any():
+                    warnings.warn("Values in x were outside bounds during a "
+                                  "minimize step, clipping to bounds", RuntimeWarning)
                 fobj, fcon, fail = self._masterFunc(np.clip(x, blx, bux), ["fobj", "fcon"])
                 f = fobj
                 g[0:m] = -fcon
