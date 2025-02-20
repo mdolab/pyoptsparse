2a5225ef9a6deae630e6151b4226164b734deba0
Alasdair Gray <alachris@umich.edu>
2025-02-20 18:31:59 -0500

    flake8 fixes

--- pyoptsparse/pyParOpt/ParOpt.py
+++ pyoptsparse/pyParOpt/ParOpt.py
@@ -1,14 +1,24 @@
 # First party modules
 from pyoptsparse.pyOpt_error import Error
+from pyoptsparse.pyOpt_optimizer import Optimizer
 
 try:
     # External modules
     from paropt.paropt_pyoptsparse import ParOptSparse as ParOpt
-except:
+except ImportError:
 
-    class ParOpt:
+    class ParOpt(Optimizer):
         def __init__(self, raiseError=True, options={}):
             name = "ParOpt"
             category = "Local Optimizer"
+            self.set_options = {}
+            self.informs = {}
+            super().__init__(
+                name,
+                category,
+                defaultOptions=self.defOpts,
+                informs=self.informs,
+                options=options,
+            )
             if raiseError:
                 raise Error("There was an error importing ParOpt")
