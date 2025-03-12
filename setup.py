0791cddc0e7a96ff2b760c0850239b6826473c62
Ella Wu <602725+ewu63@users.noreply.github.com>
2025-03-12 00:11:34 -0700

    update build system

--- setup.py
+++ setup.py
@@ -105,7 +105,7 @@ if __name__ == "__main__":
                 "matplotlib",
             ],
             "docs": docs_require,
-            "testing": ["testflo>=1.4.5", "parameterized"],
+            "testing": ["testflo>=1.4.5", "parameterized", "cyipopt"],
         },
         classifiers=[
             "Development Status :: 5 - Production/Stable",
