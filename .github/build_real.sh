6a7c907166180d9e7e23843ab8216ee1b5d4fbc3
Ella Wu <602725+ewu63@users.noreply.github.com>
2025-03-12 00:33:48 -0700

    set PKG_CONFIG_PATH

--- .github/build_real.sh
+++ .github/build_real.sh
@@ -11,6 +11,9 @@ fi
 mv ~/.config/pip/constraints.txt ~/.config/pip/constraints.txt.bkup
 touch  ~/.config/pip/constraints.txt
 
+# set $PKG_CONFIG_PATH so pkg-config can find ipopt
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$IPOPT_DIR/lib/pkgconfig
+
 pip install .[optview,testing] -v
 
 # move pip constraints file back
