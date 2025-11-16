0b67d7da79daa30added51b384a86ce070e82a00
Ella Wu <602725+ewu63@users.noreply.github.com>
2025-11-16 13:19:48 -0800

    more

--- pyoptsparse/pyOpt_types.py
+++ pyoptsparse/pyOpt_types.py
@@ -1,14 +1,14 @@
 # Standard Python modules
-from typing import Sequence, Union
+from typing import Sequence
 
 # External modules
 import numpy as np
 import numpy.typing as npt
 
 # Either ndarray or scalar
-NumpyType = Union[float, npt.NDArray[np.float64]]
+NumpyType = float | npt.NDArray[np.float64]
 # ndarray, list of numbers, or scalar
-ArrayType = Union[NumpyType, Sequence[float]]
+ArrayType = NumpyType | Sequence[float]
 # funcs
 Dict1DType = dict[str, npt.NDArray[np.float64]]
 # funcsSens
