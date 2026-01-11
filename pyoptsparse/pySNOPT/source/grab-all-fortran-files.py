1d686bdee4a8513943ff55d2721404d91ed45db4
Ella Wu <602725+ewu63@users.noreply.github.com>
2026-01-10 21:42:52 -0800

    make grab-all-fortran-files.py take arg for path

--- pyoptsparse/pySNOPT/source/grab-all-fortran-files.py
+++ pyoptsparse/pySNOPT/source/grab-all-fortran-files.py
@@ -3,15 +3,28 @@
 # Standard Python modules
 import os
 from pathlib import Path
+from argparse import ArgumentParser
 
 CURDIR = os.path.abspath(os.path.dirname(__file__))
 
-TO_SKIP = [
-    "sn27lu77.f",
-    "sn27lu90.f",
-    "snopth.f",
-]
 
-for path in Path(CURDIR).glob("*.f"):
-    if os.path.basename(path) not in TO_SKIP:
-        print(path)
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("sourcedir", type=Path, default=None)
+    args = parser.parse_args()
+    if args.sourcedir is None:
+        args.sourcedir = CURDIR
+
+    TO_SKIP = [
+        "sn27lu77.f",
+        "sn27lu90.f",
+        "snopth.f",
+    ]
+
+    for path in Path(args.sourcedir).glob("*.f"):
+        if os.path.basename(path) not in TO_SKIP:
+            print(path)
+
+
+if __name__ == "__main__":
+    main()
